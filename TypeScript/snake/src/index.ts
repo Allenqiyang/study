f4efe89f6578cd8c9c678706af76fd2d48d8af58
Allenqiyang <89894318+Allenqiyang@users.noreply.github.com>
2022-04-17 21:39:29 +0800

    Add files via upload

--- TypeScript/snake/src/index.ts
+++ TypeScript/snake/src/index.ts
@@ -1,48 +1,7 @@
 import './style/index.less'
 
-class Food{
-    element: HTMLElement
-    constructor(){
-        this.element = document.getElementById("food")!
-    }
+import Food from './modules/food'
+import scorePanel from './modules/ScorePanel'
 
-    get X(){
-        return this.element.offsetLeft
-    }
-    get Y(){
-        return this.element.offsetTop
-    }
-    change(){
-        let top = Math.floor(Math.random() * 30) * 10
-        let left = Math.floor(Math.random() * 30) * 10
-        this.element.style.top = top + "px"
-        this.element.style.left = left + "px"
-    }
-}
-
-class scorePanel{
-    score:number = 0
-    level:number = 1
-    scoreEle: HTMLElement
-    levelEle: HTMLElement
-    maxLevel: number
-    constructor(maxLevel: number = 10){
-        this.scoreEle = document.getElementById("score")!   //！表示不可能为空
-        this.levelEle = document.getElementById("level")!
-        this.maxLevel = maxLevel
-    }
-    addScore(){
-        this.score++
-        this.scoreEle.innerHTML = this.score + ''
-    }
-    levelUp(){
-        if(this.level < this.maxLevel){
-            this.level++
-            this.levelEle.innerHTML = this.level + ''
-        }
-    }
-}
-
+const food = new Food()
 const ScorePanel = new scorePanel()
-ScorePanel.addScore()
-ScorePanel.addScore()
