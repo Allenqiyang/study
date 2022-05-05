c5405cc2628af2c77df347f842f0b98f19452fd9
Allenqiyang <89894318+Allenqiyang@users.noreply.github.com>
2022-05-05 18:23:40 +0800

    Add files via upload

---
+++ JavaScript/tear codes/new.js
@@ -0,0 +1,19 @@
+function myNew(){
+    console.log(arguments)
+    //把构造函数拿出来
+    let constructor = Array.prototype.shift.apply(arguments)
+    //如果不是函数就报错
+    if(typeof constructor !== 'function'){
+        console.error('wrong')
+        return
+    }
+    // 创建一个新对象
+    let newObj = Object.create(constructor.prototype)
+    // 将this指向创建的新对象，执行构造函数代码
+    let result = constructor.apply(newObj,arguments)
+    // 判断是否为非空对象
+    let flag = result && (typeof result === 'object' || typeof result === 'function')
+    return flag ? result : newObj
+}
+
+/* myNew(构造函数,参数) */
