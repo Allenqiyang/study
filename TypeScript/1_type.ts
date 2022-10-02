a1a0b854126c3a533dfb358340c5dd008bf73a23
AllenXie <89894318+Allenqiyang@users.noreply.github.com>
2022-10-02 23:22:32 +0800

    Delete 1_type.ts

--- TypeScript/1_type.ts
+++
@@ -1,9 +0,0 @@
-function add(n1: number|string, n2: number|string) {
-  if(typeof n1 === 'number' && typeof n2 === 'number') {
-    return n1 + n2
-  } else if(typeof n1 === 'string' && typeof n2 === 'string') {
-    return n1 + n2
-  }
-}
-
-const result = add(10, 20)
